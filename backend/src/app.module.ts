cb9a58f9c6f932fc54e3acee2bd65f741f4f7570
zolbayars <zolube@gmail.com>
2022-10-08 18:00:57 +0800

    feat: connection bootstrap to mysql db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [ReportsModule, ConfigModule.forRoot()],
+  imports: [
+    ReportsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as unknown as number,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: process.env.DB_SYNCHRONIZE as unknown as boolean,
+    }),
+  ],
 })
 export class AppModule {}
